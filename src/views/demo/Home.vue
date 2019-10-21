bfed4e00c0c4a7ddb6c074e4214598e7bbf20238
xiaohua <ytoforever@aliyun.com>
2019-10-21 16:41:28 +0800

    添加demo

--- src/views/demo/Home.vue
+++ src/views/demo/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo"
-         src="../assets/logo.png">
+         src="../../assets/logo.png">
     <HelloWorld msg="欢迎使用facade vue框架" />
   </div>
 </template>
