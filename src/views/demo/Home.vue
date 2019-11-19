36054bfdba301a43fa47590b7efaac7c05e1a2df
xiaohua <ytoforever@aliyun.com>
2019-11-19 13:19:24 +0800

    优化

--- src/views/demo/Home.vue
+++ src/views/demo/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png" />
+    <img alt="Vue logo"
+         src="../../assets/logo.png" />
     <HelloWorld msg="欢迎使用facade vue框架" />
   </div>
 </template>
@@ -9,24 +10,30 @@
 import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 import app from '../../utils/app'
+import { config } from '../../utils/config'
+
 @Component({
   components: {
     HelloWorld
   }
 })
 export default class Home extends Vue {
-  public async mounted() {
+  public async created() {
     await this.initSocket()
+  }
+  // public async mounted() {}
+  public async initSocket() {
+    const baseUrl = config.BaseUrl + '/signalr'
+    const token = ''
+    const qs = 'enc_auth_token=' + encodeURIComponent(token)
+    const c = await app.socket.connect(baseUrl, qs)
+
+    // 接受websocket 消息
     app.socket.receiveMessage('DemoReceived', (name, message) => {
       console.log(name)
       console.log(message)
     })
-  }
-  public async initSocket() {
-    const baseUrl = 'http://39.98.48.213:8001/pims/signalr'
-    const token = ''
-    const qs = 'enc_auth_token=' + encodeURIComponent(token)
-    const c = await app.socket.connect(baseUrl, qs)
+    // 发送websocket 消息
     app.socket.sendMessage('Demo', 'jjj').then((res) => {
       console.log(res)
     })
