e9e663fe674e5db8ffb7a8b7327dfb1b282e39d3
xiaohua <ytoforever@aliyun.com>
2020-03-25 19:58:33 +0800

    .

--- src/views/demo/Home.vue
+++ src/views/demo/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png" />
+    <img alt="Vue logo"
+         src="../../assets/logo.png" />
     <HelloWorld msg="欢迎使用facade vue框架" />
   </div>
 </template>
